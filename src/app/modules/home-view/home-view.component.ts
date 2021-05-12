887a9d9cd7404134d7f265b1f68538872ac0209b
ChoyaSpadin <choya.spadin@gmail.com>
2021-05-12 02:25:46 -0300

    style: hero section font size

--- src/app/modules/home-view/home-view.component.ts
+++ src/app/modules/home-view/home-view.component.ts
@@ -3,6 +3,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
     selector: 'home-view',
     templateUrl: './home-view.component.html',
+    styleUrls: ['./home-view.component.scss'],
     encapsulation: ViewEncapsulation.None,
 })
 export class HomeViewComponent {}
