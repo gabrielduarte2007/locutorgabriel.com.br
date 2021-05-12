fd99b55fac00a715d8e4ba610ca0b6e903d441a5
ChoyaSpadin <choya.spadin@gmail.com>
2021-05-12 03:50:51 -0300

    style: search bar (wip)

--- src/app/modules/home-view/home-view.module.ts
+++ src/app/modules/home-view/home-view.module.ts
@@ -6,6 +6,7 @@ import { SharedModule } from 'app/shared/shared.module';
 import { HomeViewComponent } from 'app/modules/home-view/home-view.component';
 import { RouterModule } from '@angular/router';
 import { homeViewRoutes } from './home-view.routing';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
     declarations: [HomeViewComponent],
@@ -20,6 +21,7 @@ import { homeViewRoutes } from './home-view.routing';
 
         // Shared
         SharedModule,
+        MatFormFieldModule,
     ],
 })
 export class HomeViewModule {}
