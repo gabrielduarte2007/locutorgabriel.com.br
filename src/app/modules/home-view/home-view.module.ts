7fb4f58c99809941ba7e827017b97967b4a76974
ChoyaSpadin <choya.spadin@gmail.com>
2021-05-21 19:03:45 -0300

    feat: search-area component created

--- src/app/modules/home-view/home-view.module.ts
+++ src/app/modules/home-view/home-view.module.ts
@@ -10,9 +10,10 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { MatIconModule } from '@angular/material/icon';
 import { CardsAreaComponent } from './cards-area/cards-area.component';
+import { SearchAreaComponent } from './search-area/search-area.component';
 
 @NgModule({
-    declarations: [HomeViewComponent, FooterComponent, HeaderComponent, CardsAreaComponent],
+    declarations: [HomeViewComponent, FooterComponent, HeaderComponent, CardsAreaComponent, SearchAreaComponent],
     imports: [
         // Angular
         CommonModule,
