7c44fecf0fd18432be3f47389d60ed3f49eac930
ChoyaSpadin <choya.spadin@gmail.com>
2021-07-01 19:34:07 -0300

    feat: header button (WIP)

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -5,4 +5,8 @@ import { Component } from '@angular/core';
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.sass'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+    menuInfo: boolean = true;
+
+}
