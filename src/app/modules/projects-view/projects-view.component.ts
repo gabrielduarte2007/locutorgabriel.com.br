ca0b4186deb1773f426c346a51fb6fceb3f6c91c
Paulo Salvatore <salvatoregames@gmail.com>
2021-07-06 14:30:01 -0300

    fix: load project view when clicking from 'relatedProject'

--- src/app/modules/projects-view/projects-view.component.ts
+++ src/app/modules/projects-view/projects-view.component.ts
@@ -1,11 +1,13 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnDestroy, ViewEncapsulation } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { projects } from '../../../_data/projects.data';
 import slugify from 'slugify';
 import { Project } from '../../../_model/Project';
 import { ProjectsService } from '../../_services/projects.service';
 import { config } from '../../../_data/config.data';
 import { ProjectType } from '_model/ProjectType';
+import { delay, filter, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
     selector: 'projects-view',
@@ -13,19 +15,35 @@ import { ProjectType } from '_model/ProjectType';
     styleUrls: ['./projects-view.component.sass'],
     encapsulation: ViewEncapsulation.None,
 })
-export class ProjectsViewComponent {
+export class ProjectsViewComponent implements OnDestroy {
     public currentProject: Project;
 
     public relatedProjects: Project[] = projects.slice(20, 26);
 
-    public visibleShareLink: boolean = false;
+    public visibleShareLink = false;
 
     public ProjectType = ProjectType;
+
+    private _unsubscribeAll: Subject<any> = new Subject<any>();
 
     constructor(
         private readonly router: Router,
         public readonly service: ProjectsService,
     ) {
+        this.loadCurrentProject();
+
+        this.router.events
+            .pipe(
+                takeUntil(this._unsubscribeAll),
+                delay(0),
+                filter(event => event instanceof NavigationEnd),
+            )
+            .subscribe(() => {
+                this.loadCurrentProject();
+            });
+    }
+
+    private loadCurrentProject(): void {
         const slug = this.router.url?.replace('/', '');
 
         this.currentProject = projects.find(
@@ -57,7 +75,14 @@ export class ProjectsViewComponent {
         );
     }
 
-    shareLink(): void {
+    public shareLink(): void {
         this.visibleShareLink = !this.visibleShareLink;
+    }
+
+    public ngOnDestroy(): void {
+        console.log('destroy');
+
+        this._unsubscribeAll.next();
+        this._unsubscribeAll.complete();
     }
 }
