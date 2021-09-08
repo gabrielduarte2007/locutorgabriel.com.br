8223c340ef3e795b4023c7ea4189b7ec0c038d19
Paulo Salvatore <salvatoregames@gmail.com>
2021-09-07 21:44:36 -0300

    feat(search): implement isotope filtering

--- src/app/shared/cards-section/project-list/project-list.component.ts
+++ src/app/shared/cards-section/project-list/project-list.component.ts
@@ -1,24 +1,47 @@
-import { Component, Input } from '@angular/core';
+import {
+    AfterViewInit,
+    ChangeDetectionStrategy,
+    Component,
+    ElementRef,
+    Input,
+    ViewChild,
+} from '@angular/core';
 import { Project } from '../../../../_model/Project';
 import { ProjectsService } from '../../../_services/projects.service';
 import { FilterService } from '../../../_services/filter.service';
+import * as Isotope from 'assets/libs/isotope.pkgd.min';
 
 @Component({
     selector: 'app-project-list',
     templateUrl: './project-list.component.html',
     styleUrls: ['./project-list.component.sass'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProjectListComponent {
+export class ProjectListComponent implements AfterViewInit {
     @Input()
     public projects: Project[];
+
+    @ViewChild('projectsElement')
+    projectsElement: ElementRef<HTMLDivElement>;
+
+    public isotopeInstance: any;
 
     constructor(
         public readonly service: ProjectsService,
         public readonly filterService: FilterService,
     ) {}
 
-    public updateList(projects: Project[]): void {
-        this.projects = projects;
+    public filterProjects(filteredProjects: Project[]): void {
+        setTimeout(() => {
+            this.isotopeInstance.arrange({
+                filter: itemElem => {
+                    const index = itemElem.getAttribute('data-index');
+                    const project = this.projects[index];
+
+                    return filteredProjects.indexOf(project) >= 0;
+                },
+            });
+        }, 0);
     }
 
     public getTagList(project: Project): string[] {
@@ -30,5 +53,12 @@ export class ProjectListComponent {
 
         $event.stopPropagation();
         $event.preventDefault();
+    }
+
+    ngAfterViewInit(): void {
+        const elem = this.projectsElement.nativeElement;
+        this.isotopeInstance = new Isotope(elem, {
+            itemSelector: '.card',
+        });
     }
 }
