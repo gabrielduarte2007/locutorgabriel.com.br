6c9a514292c9f9883e40f02bcfcedc9cd53262ed
Paulo Salvatore <salvatoregames@gmail.com>
2021-09-08 00:35:29 -0300

    feat(logo): add polyline

--- src/app/modules/home-view/home-view.component.ts
+++ src/app/modules/home-view/home-view.component.ts
@@ -1,4 +1,10 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import {
+    AfterViewInit,
+    Component,
+    ElementRef,
+    ViewChild,
+    ViewEncapsulation,
+} from '@angular/core';
 import { projects } from '../../../_data/projects.data';
 
 @Component({
@@ -7,6 +13,60 @@ import { projects } from '../../../_data/projects.data';
     styleUrls: ['./home-view.component.sass'],
     encapsulation: ViewEncapsulation.None,
 })
-export class HomeViewComponent {
+export class HomeViewComponent implements AfterViewInit {
     public projects = projects;
+
+    @ViewChild('polylineStart')
+    polylineStart: ElementRef<HTMLImageElement>;
+
+    @ViewChild('polylineEnd')
+    polylineEnd: ElementRef<HTMLDivElement>;
+
+    @ViewChild('polyline')
+    polyline: ElementRef<SVGPolylineElement>;
+
+    @ViewChild('circle')
+    circle: ElementRef<SVGCircleElement>;
+
+    ngAfterViewInit(): void {}
+
+    addPolyline(): void {
+        const circle = this.circle.nativeElement;
+        const polyline = this.polyline.nativeElement;
+        const svg = polyline.ownerSVGElement;
+
+        polyline.points.clear();
+
+        // Get Start Position
+        const polylineStart = this.polylineStart.nativeElement;
+
+        const xStart = polylineStart.offsetLeft + polylineStart.width - 40;
+        const yStart = polylineStart.offsetTop + polylineStart.height / 2;
+
+        // Add Circle at Start Position
+        circle.setAttributeNS(null, 'cx', xStart.toString());
+        circle.setAttributeNS(null, 'cy', yStart.toString());
+
+        // Add Point at Start Position
+        const pointStart: SVGPoint = svg.createSVGPoint();
+        pointStart.x = xStart;
+        pointStart.y = yStart;
+        polyline.points.appendItem(pointStart);
+
+        // Get End Position
+        const polylineEnd = this.polylineEnd.nativeElement;
+
+        const xEnd = polylineEnd.offsetLeft;
+        const yEnd = polylineEnd.offsetTop + polylineEnd.clientHeight + 1;
+
+        // Add Point at End Position
+        const pointEnd: SVGPoint = svg.createSVGPoint();
+        pointEnd.x = xEnd;
+        pointEnd.y = yEnd;
+        polyline.points.appendItem(pointEnd);
+    }
+
+    onResize(): void {
+        this.addPolyline();
+    }
 }
