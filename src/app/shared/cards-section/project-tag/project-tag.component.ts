b455415536ed9bca30cfa1682bbee99834d3dec1
Daniel Andrade <andraddedan@icloud.com>
2022-02-28 03:19:42 -0300

    fix: ajusta scroller e adição de tags

--- src/app/shared/cards-section/project-tag/project-tag.component.ts
+++ src/app/shared/cards-section/project-tag/project-tag.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { FilterService } from '../../../_services/filter.service';
 import { ProjectTag } from '../../../../_model/ProjectTag';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
     selector: 'app-project-tag',
@@ -14,10 +15,14 @@ export class ProjectTagComponent {
     @Input()
     public rawTag: string;
 
-    constructor(private readonly filterService: FilterService) {}
+    constructor(
+        private readonly filterService: FilterService,
+        private readonly viewportScroller: ViewportScroller
+    ) {}
 
     public onClickTag($event: MouseEvent): void {
-        this.filterService.addTag(this.tag.text);
+        this.filterService.addTag(this.tag?.text || this.rawTag);
+        this.viewportScroller.scrollToAnchor('cards-filter');
 
         $event.stopPropagation();
         $event.preventDefault();
